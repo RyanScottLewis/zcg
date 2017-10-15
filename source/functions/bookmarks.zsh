e660cbebe8ce95ea865d0d1d059adf33b55defcf
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2017-10-15 01:33:42 -0400

    Update bm to allow setting bookmarks directory

--- source/functions/bookmarks.zsh
+++ source/functions/bookmarks.zsh
@@ -1,24 +1,25 @@
 # TODO: Use $BOOKMARKS path based on hostname
+export BOOKMARKS_PATH="/Volumes/Media/Self/Science/Technology/Software/Bookmarks"
 
 # Change directory to programming bookmark, or the bookmarks directory if none is given
 function bm {
-  cd "/Volumes/Media/Self/Science/Technology/Software Engineering/Bookmarks/$1"
+  cd "$BOOKMARKS_PATH/$1"
 }
 
 # Add a bookmark
 function bmadd {
   if [ ! -z "$1" ]; then
-    ln -s "../$1" "/Volumes/Media/Self/Science/Technology/Software Engineering/Bookmarks/"
+    ln -s "../$1" "$BOOKMARKS_PATH/"
   fi
 }
 
 # Remove a bookmark
 function bmrm {
   if [ ! -z "$1" ]; then
-    rm "/Volumes/Media/Self/Science/Technology/Software Engineering/Bookmarks/$1"
+    rm "$BOOKMARKS_PATH/$1"
   fi
 }
 
 function bmls {
-  ls "/Volumes/Media/Self/Science/Technology/Software Engineering/Bookmarks" | col | sort
+  ls "$BOOKMARKS_PATH" | col | sort
 }
