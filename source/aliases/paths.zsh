8da90164e0a2ad8bfc5201b7bca5910f37200d61
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2017-10-15 01:33:28 -0400

    Remove paths.zsh

--- source/aliases/paths.zsh
+++
@@ -1,7 +0,0 @@
-if [ "$HOST" = "RyMacMini.local" ]; then
-  export MEDIA_PATH='/Volumes/Media'
-else
-  export MEDIA_PATH='/mnt/Media'
-fi
-
-alias code="cd '$MEDIA_PATH/Self/Science/Technology/Software Engineering'"
