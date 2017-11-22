524b040fa92c2b9582d13a1d05b65f72f04083c8
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2017-11-22 03:51:46 -0500

    Use Linux as default ls alias

--- source/aliases/ls.zsh
+++ source/aliases/ls.zsh
@@ -1,9 +1,9 @@
 UNAME=`uname`
 
-if [[ "$UNAME_STR" == 'Linux' ]]; then
-  alias l="ls -al --color=auto"
-elif [[ "$UNAME_STR" == 'Darwin' || "$UNAME_STR" == 'FreeBSD' ]]; then
-  alias l="ls -alG"
+if [[ "$UNAME_STR" == 'Darwin' || "$UNAME_STR" == 'FreeBSD' ]]; then
+  alias l="ls -alhG"
+else
+  alias l="ls -alh --color=auto"
 fi
 
 unset UNAME_STR
