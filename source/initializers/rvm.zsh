95f678543fe6f57469aa9bfe94753555ab92677b
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2017-11-21 22:06:30 -0500

    Refactor RVM to to only work when a ~/.rvm dir is found

--- source/initializers/rvm.zsh
+++ source/initializers/rvm.zsh
@@ -1,4 +1,7 @@
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-# export PATH="$PATH:$HOME/.gem/ruby/2.4.0/bin" # Add RVM to PATH for scripting
+# Setup RVM, if there is a ~/.rvm directory
 
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+if [[ -x "$HOME/.rvm" ]]; then
+  export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+fi
